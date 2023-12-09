2eea972e3d4e15227941d611f468606aa55ea26d
JosephYM37 <josech@umich.edu>
2023-12-09 18:49:57 -0500

    delete db

--- League_of_Legends.py
+++ League_of_Legends.py
@@ -13,7 +13,7 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Players
                   (Name TEXT, Rank INT, LP INT)''')
 
-<<<<<<< HEAD
+
 # Insert data into the table
 for player in data['players']:
     cursor.execute("INSERT INTO Players (Name, Rank, LP) VALUES (?, ?, ?)",
@@ -24,20 +24,3 @@ conn.commit()
 conn.close()
 
 print("Data inserted into SQLite database.")
-=======
-api_url_matchid = "https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4836852673"
-
-api_url_matchid = api_url_matchid + '?api_key=' + api_key
-resp = requests.get(api_url_matchid)
-match_data = resp.json()
-
-
-player_data = match_data['info']['participants'][0]
-k = player_data['kills']
-d = player_data['deaths']
-a = player_data['assists']
-print("Kills:", k)
-print("Deaths:", d)
-print("Assists:", a)
-print("KDA:", (k + a) / d)
->>>>>>> refs/remotes/origin/main
