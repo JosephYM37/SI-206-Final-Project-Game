172b3f11963bcd1c899698467ea51189b6019e27
JosephYM37 <josech@umich.edu>
2023-12-09 18:45:08 -0500

    db addition

--- League_of_Legends.py
+++ League_of_Legends.py
@@ -1,36 +1,25 @@
-import unittest
-import sqlite3
 import json
-import os
-import matplotlib.pyplot as plt
-import requests
+import sqlite3
 
-api_key = "RGAPI-5ccc7cc0-ad7d-4fe3-b577-2f46a827bbc4"
-api_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MonkyPeen"
-api_url = api_url + '?api_key=' + api_key
+# Read data from JSON file
+with open('league_data.json', 'r') as file:
+    data = json.load(file)
 
-resp = requests.get(api_url)
-player_info = resp.json()
-print(player_info)
+# Create or connect to an SQLite database
+conn = sqlite3.connect('top_players.db')
+cursor = conn.cursor()
 
-api_url_match = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/irE6cW5IhU_qNdil0T9Fy7G44rhZAWvy85UTOsf6R3bGv3A-_TXDqxKQZiIUGZe9oynKIHfgF5VTMg/ids?start=0&count=20"
-api_url_match = api_url_match + '&api_key=' + api_key 
-resp = requests.get(api_url_match)
-match_ids = resp.json()
-print(match_ids)
+# Create a table
+cursor.execute('''CREATE TABLE IF NOT EXISTS Players
+                  (Name TEXT, Rank INT, LP INT)''')
 
-api_url_matchid = "https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4836852673"
-# This one doesn't have any arguments (see, there's no "?" above) so we'll add it with the api key
-api_url_matchid = api_url_matchid + '?api_key=' + api_key
-resp = requests.get(api_url_matchid)
-match_data = resp.json()
+# Insert data into the table
+for player in data['players']:
+    cursor.execute("INSERT INTO Players (Name, Rank, LP) VALUES (?, ?, ?)",
+                   (player['name'], player['rank'], player['lp']))
 
-# To save time, we'll assign a variable for the first player
-player_data = match_data['info']['participants'][0]
-k = player_data['kills']
-d = player_data['deaths']
-a = player_data['assists']
-print("Kills:", k)
-print("Deaths:", d)
-print("Assists:", a)
-print("KDA:", (k + a) / d)
+# Commit changes and close connection
+conn.commit()
+conn.close()
+
+print("Data inserted into SQLite database.")
