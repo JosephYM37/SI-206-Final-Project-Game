42a0b8343052792cf95626bf5c2a7c86df3939c7
DiurnalDays <amjiao@umich.edu>
2023-12-09 20:29:31 -0500

    database implementation

--- RAWG.py
+++ RAWG.py
@@ -9,21 +9,36 @@ api_key = "34cf19bf45874c2c9d0ed8defa498b17"
 api_url = f"https://api.rawg.io/api/games"
 api_url = api_url + '?key=' + api_key
 
-game_name = 'League of Legends'
+# Define the tag and ordering
+tag = 'MOBA'
+ordering = '-rating'
 
-response = requests.get(f'{api_url}&search={game_name}')
- 
+# Make the API request
+response = requests.get(f'{api_url}&tags={tag}&ordering={ordering}&page_size=25')
+
+# Parse the JSON response
 data = response.json()
-game_info = data['results'][0]
+games_info = data['results']
 
+# Connect to the SQLite database
 conn = sqlite3.connect('games_database.db')
 
+# Create a cursor object
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS League_of_Legends
+# Create table
+cursor.execute('''CREATE TABLE IF NOT EXISTS MOBA_Games
                   (Id INT, Name TEXT, Rating REAL, Playtime INT)''')
 
-cursor.execute("INSERT INTO League_of_Legends (id, name, rating, playtime) VALUES (?, ?, ?, ?)",
-               (game_info['id'], game_info['name'], game_info['rating'], game_info['playtime']))
+# Insert the games' information into the database
+for game_info in games_info:
+    cursor.execute("SELECT * FROM MOBA_Games WHERE Id = ?", (game_info['id'],))
+    game_in_db = cursor.fetchone()
+    if game_in_db is None:
+        cursor.execute("INSERT INTO MOBA_Games (Id, Name, Rating, Playtime) VALUES (?, ?, ?, ?)",
+                       (game_info['id'], game_info['name'], game_info['rating'], game_info['playtime']))
+# Commit the changes
 conn.commit()
+
+# Close the connection
 conn.close()
