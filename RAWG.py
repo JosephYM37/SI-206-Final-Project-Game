f0fcce4da3586ce8a9358ef75b8a007427b8d0ed
DiurnalDays <amjiao@umich.edu>
2023-12-09 19:44:08 -0500

    bweh

--- RAWG.py
+++ RAWG.py
@@ -10,3 +10,14 @@ api_url = "https://api.rawg.io/api/games"
 api_url = api_url + '?key=' + api_key
 
 resp = requests.get(api_url)
+
+url = "https://rawg-video-games-database.p.rapidapi.com/games/league-of-legends"
+
+headers = {
+	"X-RapidAPI-Key": "bd83204c63msh395f771e02eb0ddp15ced2jsna2072c4e0f3e",
+	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
