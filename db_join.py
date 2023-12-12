6642993abc2eea9c2f249f231472850e9ba9224e
DiurnalDays <amjiao@umich.edu>
2023-12-11 21:15:26 -0500

    Update db_join.py

--- db_join.py
+++ db_join.py
@@ -12,15 +12,7 @@ cursor.execute('''
     JOIN GamePlaytimes ON GameNames.ID = GamePlaytimes.ID
 ''')
 
+
 # Commit changes to the database
 conn.commit()
 conn.close()
-
-#Calculate average playtime per game
-results = cursor.fetchall()
-total_playtime = sum(row[1] for row in results)
-average_playtime = total_playtime / len(results)
-
-#Write average playtime to txt file
-with open('average_playtime.txt', 'w') as f:
-    f.write(f'Average playtime: {average_playtime}\n')
