7caf73e8c7f112ae6c8b58bce4da9cd0c79c7710
DiurnalDays <amjiao@umich.edu>
2023-12-11 22:56:52 -0500

    created visualizations

--- data_visualizations.py
+++ data_visualizations.py
@@ -8,14 +8,19 @@ conn = sqlite3.connect('games_database.db')
 cursor = conn.cursor()
 
 cursor.execute("SELECT Playtime FROM JoinedData WHERE Name = 'League of Legends'")
-lol_playtimes = [row[0] for row in cursor.fetchall()]
+result = cursor.fetchone()
+lol_playtime = result[0] if result else None
 
-cursor.execute("SELECT Playtime FROM JoinedData WHERE Name = 'Dota2'")
-dota2_playtimes = [row[0] for row in cursor.fetchall()]
+cursor.execute("SELECT Playtime FROM JoinedData WHERE Name = 'Dota 2'")
+result = cursor.fetchone()
+dota2_playtime = result[0] if result else None
 
-playtimes = lol_playtimes + dota2_playtimes
-plt.bar(['League of Legends', 'Dota2'], playtimes)
+plt.figure(figsize=(10, 10))
+plt.bar(['League of Legends', 'Dota2'], [lol_playtime, dota2_playtime], color=['DeepSkyBlue','green'])
 plt.xlabel('Game')
 plt.ylabel('Average Playtime (hours)')
-plt.title('Average Playtimes of League of Legends and Dota2')
+plt.title('Average Self-Reported Playtimes of League of Legends and Dota2')
+
+plt.yticks(range(0, 125, 5))
+
 plt.show()
