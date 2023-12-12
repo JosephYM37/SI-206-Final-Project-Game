6998e7d36890123d10e998d52e8c69adf92fcef5
DiurnalDays <amjiao@umich.edu>
2023-12-11 22:13:15 -0500

    visualization

--- data_visualizations.py
+++ data_visualizations.py
@@ -4,5 +4,18 @@ import sqlite3
 import requests
 import matplotlib.pyplot as plt
 
-conn = sqlite3.connect('your_database.db')
+conn = sqlite3.connect('games_database.db')
 cursor = conn.cursor()
+
+cursor.execute("SELECT Playtime FROM JoinedData WHERE Name = 'League of Legends'")
+lol_playtimes = [row[0] for row in cursor.fetchall()]
+
+cursor.execute("SELECT Playtime FROM JoinedData WHERE Name = 'Dota2'")
+dota2_playtimes = [row[0] for row in cursor.fetchall()]
+
+playtimes = lol_playtimes + dota2_playtimes
+plt.bar(['League of Legends', 'Dota2'], playtimes)
+plt.xlabel('Game')
+plt.ylabel('Average Playtime (hours)')
+plt.title('Average Playtimes of League of Legends and Dota2')
+plt.show()
