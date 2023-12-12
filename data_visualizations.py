db77cc941f1e18e5f05f1716e8cad2c9b621fad7
DiurnalDays <amjiao@umich.edu>
2023-12-12 15:50:15 -0500

    separate visualizations

--- data_visualizations.py
+++ data_visualizations.py
@@ -17,8 +17,6 @@ cursor.execute("SELECT Playtime FROM JoinedData WHERE Name = 'Dota 2'")
 result = cursor.fetchone()
 dota2_playtime = result[0] if result else None
 
-# Create a figure and a set of subplots
-fig, axs = plt.subplots(3, 2, figsize=(15, 15))  # 3 rows, 2 columns 
 
 # First subplot
 axs[0, 0].bar(['League of Legends', 'Dota2'], [lol_playtime, dota2_playtime], color=['DeepSkyBlue','green'])
@@ -27,23 +25,24 @@ axs[0, 0].set_ylabel('Average Playtime (hours)')
 axs[0, 0].set_title('Average Self-Reported Playtimes of League of Legends and Dota2')
 axs[0, 0].set_yticks(range(0, 125, 5))
 
-# Connect to SQLite database
+ #data visualiztion
+    # Connect to SQLite database
 conn = sqlite3.connect('dota2_db.sqlite')
 
-# Execute the query and convert it into a pandas DataFrame
+    # Execute the query and convert it into a pandas DataFrame
 df_public_matches = pd.read_sql_query("SELECT * from publicMatches", conn)
 df_pro_matches = pd.read_sql_query("SELECT * from proMatches", conn)
 
-# Calculate the average duration for public and pro matches
+    # Calculate the average duration for public and pro matches
+    # The duration column is currently a string in the format "HH:MM:SS". We need to convert it to seconds for calculation.
 df_public_matches['duration_seconds'] = pd.to_timedelta(df_public_matches['duration']).dt.total_seconds()
 df_pro_matches['duration_seconds'] = pd.to_timedelta(df_pro_matches['duration']).dt.total_seconds()
 
-# Calculate the averages
+    # Calculate the averages
 avg_duration_public_matches = df_public_matches['duration_seconds'].mean() / 60 # convert to minutes
 avg_duration_pro_matches = df_pro_matches['duration_seconds'].mean() / 60 # convert to minutes
 
-# Second subplot
- # Plot the bar chart
+    # Plot the bar chart
 names = ['Public Matches', 'Pro Matches']
 values = [avg_duration_public_matches, avg_duration_pro_matches]
 
@@ -51,43 +50,54 @@ values = [avg_duration_public_matches, avg_duration_pro_matches]
 plt.bar(names, values, color=['blue', 'red'])
 plt.ylabel('Average Duration (minutes)', fontsize=12, weight='bold')
 plt.title('Comparison of Average Durations for Public and Pro Matches from OpenDota API', fontsize=14, weight='bold')
-
-axs[0, 1].bar(names, values)
-axs[0, 1].set_ylabel('Average Duration (minutes)')
-axs[0, 1].set_title('Comparison of Average Durations for Public and Pro Matches in Dota2')
-
-# Load player data into DataFrame
-df_pro_players = pd.read_sql_query("SELECT * from proPlayers", conn)
-
-# Third subplot
-axs[1, 0].hist(df_pro_players['kills'], color='blue', edgecolor='black', linewidth=1.2, bins=20)
-axs[1, 0].set_title('Histogram of Kills (proPlayers) from OpenDota API', weight='bold')
-axs[1, 0].set_xlabel('Kills', fontsize=14)
-axs[1, 0].set_ylabel('Frequency', fontsize=14)
-
-# Fourth subplot
-axs[1, 1].hist(df_pro_players['deaths'], color='red', edgecolor='black', linewidth=1.2, bins=20)
-axs[1, 1].set_title('Histogram of Deaths (proPlayers) from OpenDota API', weight='bold')
-axs[1, 1].set_xlabel('Deaths', fontsize=14)
-axs[1, 1].set_ylabel('Frequency', fontsize=14)
-
-# Fifth subplot
-axs[2, 0].hist(df_pro_players['assists'], color='green', edgecolor='black', linewidth=1.2, bins=20)
-axs[2, 0].set_title('Histogram of Assists (proPlayers) from OpenDota API', weight='bold')
-axs[2, 0].set_xlabel('Assists', fontsize=14)
-axs[2, 0].set_ylabel('Frequency', fontsize=14)
-
-# Sixth subplot
-axs[2, 1].hist(df_pro_players['rank_tier'], color='purple', edgecolor='black', linewidth=1.2, bins=20)
-axs[2, 1].set_title('Histogram of Rank Tiers (proPlayers) from OpenDota API', weight='bold')
-axs[2, 1].set_xlabel('Rank Tier', fontsize=14)
-axs[2, 1].set_ylabel('Frequency', fontsize=14)
-
-# Automatically adjust subplot params for better spacing
-plt.tight_layout()
-plt.subplots_adjust(hspace=0.25, wspace=0.25)
 plt.show()
 
+    # Load player data into DataFrame
+df_pro_players = pd.read_sql_query("SELECT * from proPlayers", conn)
+
+   # For histograms
+plt.figure(figsize=(5,5))
+plt.hist(df_pro_players['kills'], color='blue', edgecolor='black', linewidth=1.2, bins=20)
+plt.title('Histogram of Kills (proPlayers) from OpenDota API', weight='bold')
+plt.xlabel('Kills', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.show()
+
+plt.figure(figsize=(5,5))
+plt.hist(df_pro_players['deaths'], color='red', edgecolor='black', linewidth=1.2, bins=20)
+plt.title('Histogram of Deaths (proPlayers) from OpenDota API', weight='bold')
+plt.xlabel('Deaths', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.show()
+
+plt.figure(figsize=(5,5))
+plt.hist(df_pro_players['assists'], color='green', edgecolor='black', linewidth=1.2, bins=20)
+plt.title('Histogram of Assists (proPlayers) from OpenDota API', weight='bold')
+plt.xlabel('Assists', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.show()
+
+plt.figure(figsize=(5,5))
+plt.hist(df_pro_players['rank_tier'], color='purple', edgecolor='black', linewidth=1.2, bins=20)
+plt.title('Histogram of Rank Tiers (proPlayers) from OpenDota API', weight='bold')
+plt.xlabel('Rank Tier', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.show()
+
+plt.figure(figsize=(5,5))
+plt.hist(df_pro_players['wins'], color='orange', edgecolor='black', linewidth=1.2, bins=20)
+plt.title('Histogram of Wins (proPlayers) from OpenDota API', weight='bold')
+plt.xlabel('Wins', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.show()
+
+plt.figure(figsize=(5,5))
+plt.hist(df_pro_players['losses'], color='pink', edgecolor='black', linewidth=1.2, bins=20)
+plt.title('Histogram of Losses (proPlayers) from OpenDota API', weight='bold')
+plt.xlabel('Losses', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.show()
+  
 conn.close()
 
 time_slots = ['< 10', '10 - 20', '20 - 30', '30 - 40', '40+']
